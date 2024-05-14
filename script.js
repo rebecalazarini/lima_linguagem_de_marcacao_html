7005b829ca003b7d98db069e9a8c874992881209
rebecalazarini <160655787+rebecalazarini@users.noreply.github.com>
2024-05-14 14:33:22 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function somar (){
-var numero1 = document.getElementById('numero1').value;
-var numero2 = document.getElementById('numero2').value;
-var resultado = parseFloat(numero1) + parseFloat(numero2);
-document.getElementById('resultado').innerHTML = resultado;
-
-
-    
-}
